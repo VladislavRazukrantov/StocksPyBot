bd4b48fda3c4a3ab8def4656be7e8a4338510871
Vladislav <vrazbusiness@mail.ru>
2022-10-13 11:19:11 +0300

    add start and help message and buttons

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import telebot
+from telebot import types
+
 bot = telebot.TeleBot('5707455690:AAH_dra68o20IGv0q94Bz7O1m3mHdoS73dw')
+keyboard = types.InlineKeyboardMarkup()
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    start_message = 'Привет! Это финансовый бот, который позволит тебе упростить работу с акциями!\n\n' + \
+                    'Команды:\n\n' + \
+                    '/quotations - Посмотреть котировки \n' + \
+                    '/portfolio - Посмотреть портфель\n'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    key_quotations = types.KeyboardButton(text='Котировки')
+    key_portfolio = types.KeyboardButton(text='Портфель')
+    markup.add(key_portfolio, key_quotations)
+    bot.send_message(message.chat.id, start_message, reply_markup=markup)  # третий аргумент - меняем клавиатуру на кнопочную
+
+bot.polling(none_stop=True, interval=0)
